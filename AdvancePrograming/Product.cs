18e062d3d6cf8cd72b6e8b75d2b4bd8ef72ffc36
NhanPhan99 <106041381+NhanPhan99@users.noreply.github.com>
2023-06-16 15:54:55 +0700

    push

---
+++ AdvancePrograming/Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancePrograming
+{
+	public class Product
+	{
+		List<OrderDetail> orderDetails = new List<OrderDetail>();
+
+		public int ProductID { get; set; }
+
+		public string Name { get; set; }
+
+		public double Price { get; set; }
+
+		internal List<OrderDetail> OrderDetails { get => orderDetails; set => orderDetails = value; }
+
+		public Product(int productID, string name, double price)
+		{
+			ProductID = productID;
+			Name = name;
+			Price = price;
+		}
+		public Product()
+		{
+		}
+
+		internal void AddOrderDetail(OrderDetail orderDetail)
+		{
+			OrderDetails.Add(orderDetail);
+		}
+
+		public bool RemoveOrderDetail(Order order)
+		{
+			var orderDetail = OrderDetails.FirstOrDefault(a => a.Order.Equals(order));
+			if (orderDetail == null)
+			{
+				return false;
+			}
+			OrderDetails.Remove(orderDetail);
+			return true;
+		}
+		public override string ToString()
+		{
+			return "Product ID:" + ProductID + " Name:" + Name + " Price:" + Price;
+		}
+	}
+}
